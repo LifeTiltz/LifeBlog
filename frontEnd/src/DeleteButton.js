43227e570f0b200ea85c3b3183bd8c70780a0762
LifeTiltz <85487129+LifeTiltz@users.noreply.github.com>
2022-01-17 20:42:53 +0000

    it refreshes the list of books when going back home after del something. but not in an elegant way. i will move on because there is more to do

--- frontEnd/src/DeleteButton.js
+++ frontEnd/src/DeleteButton.js
@@ -1,6 +1,7 @@
 import deleteButton from "./images/deleteButton.svg"
 import { projFireStore } from './fireBase';
 import { useHistory } from 'react-router-dom'
+import GetDataAgain from "./GetDataAgain";
 
 export default function DeleteButton({ id }) {
     const history = useHistory()
@@ -9,6 +10,8 @@ export default function DeleteButton({ id }) {
     const handleClick = (id) => {
         projFireStore.collection("books").doc(id).delete()
         history.push('/LifeBlog');
+        let test = GetDataAgain()
+        console.log(test);
     }
 
     return (
